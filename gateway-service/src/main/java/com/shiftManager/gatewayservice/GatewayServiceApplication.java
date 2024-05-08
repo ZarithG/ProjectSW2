d4e9a91015d644352c3649c21c801c49862724c6
ZarithG <zarith.gomez@uptc.edu.co>
2024-05-08 12:45:17 -0500

    advance

--- gateway-service/src/main/java/com/shiftManager/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/shiftManager/gatewayservice/GatewayServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
